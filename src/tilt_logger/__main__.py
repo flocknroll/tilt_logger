4a11286db31b945fcb222b17a98607e68af845c4
flocknroll <dosso.florent@gmail.com>
2021-06-10 08:46:04 +0200

    init async db write

--- src/tilt_logger/__main__.py
+++ src/tilt_logger/__main__.py
@@ -1,4 +1,4 @@
-import psycopg2
+import aiopg
 import aioblescan as aiobs
 import os
 import asyncio
@@ -10,30 +10,44 @@ from aioblescan.plugins.tilt import Tilt
 pwd = os.environ['TILT_DB_PWD']
 
 
-def get_packet_processor(pg_conn):
+def get_packet_processor(queue):
     def process_packet(data):
         ev = aiobs.HCI_Event()
         ev.decode(data)
         packet = Tilt().decode(ev)
         
         if packet:
-            print(packet)
-            cur = pg_conn.cursor()
-
-            cur.execute("INSERT INTO tilt.tilt_log (time, gravity, temp_farenheit, signal, battery) VALUES (%s, %s, %s, %s, %s)",
-                (datetime.now(tz=tzlocal()), packet["minor"], packet["major"], packet["rssi"], packet["tx_power"]))
-            pg_conn.commit()
+            queue.put_nowait(packet)
 
     return process_packet
+
+
+async def packet_writer(queue):
+    try:
+        while True:
+            packet = await queue.get()
+
+            async with aiopg.connect(host='localhost', dbname='tilt_db', user='tilt', password=pwd) as pg_conn:
+                cur = await pg_conn.cursor()
+                await cur.execute("INSERT INTO tilt.tilt_log (time, gravity, temp_farenheit, signal, battery) VALUES (%s, %s, %s, %s, %s)",
+                        (datetime.now(tz=tzlocal()), packet["minor"], packet["major"], packet["rssi"], packet["tx_power"]))
+
+                print(f"Wrote {packet}")
+
+                queue.task_done()
+    except asyncio.CancelledError:
+        pass
 
 
 def main():
     event_loop = asyncio.get_event_loop()
     socket = aiobs.create_bt_socket(0)
-    pg = psycopg2.connect(host='localhost', dbname='tilt_db', user='tilt', password=pwd)
+    queue = asyncio.Queue()
 
     conn, btctrl = conn, btctrl = event_loop.run_until_complete(event_loop._create_connection_transport(socket, aiobs.BLEScanRequester, None, None))
-    btctrl.process = get_packet_processor(pg)
+    btctrl.process = get_packet_processor(queue)
+
+    pg_task = event_loop.create_task(packet_writer(queue))
 
     event_loop.run_until_complete(btctrl.send_scan_request())
 
@@ -42,11 +56,17 @@ def main():
     except KeyboardInterrupt:
         print("keyboard interrupt")
     finally:
-        print("closing event loop")
         event_loop.run_until_complete(btctrl.stop_scan_request())
+        print("scan stopped")
+        event_loop.run_until_complete(queue.join())
+        print("queue empty")
+        pg_task.cancel()
+        event_loop.run_until_complete(pg_task)
+        print("packet writer task cancelled")
+
         conn.close()
-        pg.close()
         event_loop.close()
+        print("loop closed")
 
 if __name__ == "__main__":
     main()
